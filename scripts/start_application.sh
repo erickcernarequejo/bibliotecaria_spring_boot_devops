a7f48d6ff11f929cdf0da89f196e9019a545d028
Erick Edgardo Cerna Requejo <william8178@hotmail.com>
2024-10-02 22:45:32 -0500

    Actualizar inicio de la aplicación

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+
+# Carga las variables de entorno
 source /home/ec2-user/app/scripts/load_env_vars.sh
-cd /home/ec2-user/app
+
+# Directorio de la aplicación
+APP_DIR="/home/ec2-user/app"
+JAR_DIR="$APP_DIR/target"
+LOG_DIR="$APP_DIR/logs"
+
+# Nombre del archivo JAR (asume que solo hay uno)
+JAR_FILE=$(ls $JAR_DIR/*.jar | head -n 1)
+
 nohup java -jar \
       -Dserver.port=80 \
       -DDB_NAME=$DB_NAME \
       -DDB_USERNAME=$DB_USERNAME \
       -DDB_PASSWORD=$DB_PASSWORD \
-      *.jar > /dev/null 2>&1 &
+      $JAR_FILE > /dev/null 2>&1 &
+
 echo $! > ./app.pid
