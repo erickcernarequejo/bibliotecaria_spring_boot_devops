54c251a7dda1ed08ae83d0674629ad265a5ab303
Erick C.R <william8178@hotmail.com>
2024-12-21 17:32:02 -0500

    Corregir impresión logs

--- src/main/java/com/ecernare/bibliotecaria/controller/AuthorController.java
+++ src/main/java/com/ecernare/bibliotecaria/controller/AuthorController.java
@@ -20,7 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.Optional;
 
-@Tag(name = "Authors", description = "Authors Management Test")
+@Tag(name = "Authors", description = "Authors Management")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/authors")
